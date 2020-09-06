9b8f02adf966a65cd770fb6ab7a14c09bebf303c
1458206584@qq.com <1458206584@qq.com>
2020-09-06 14:55:14 +0800

    完成会员登录

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -1,7 +1,52 @@
 # -*- coding: utf-8 -*
-#Author: Alex Cater
+# Author: Alex Cater
 """
 # @Author  ：Alex Cater
 # @Time    : 2020/8/14 21:35
 # @File    ： forms.py
 """
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, FileField, TextAreaField, SelectMultipleField
+from wtforms.validators import DataRequired, EqualTo,ValidationError
+from app.models import Admin
+
+
+class LoginForm(FlaskForm):
+    """管理员登录表单"""
+    account = StringField(
+        label="账号",
+        validators=[
+            DataRequired("请输入账号！")
+        ],
+        description="账号",
+        render_kw={
+            "class": "form-control",
+            "placeholder": "请输入账号！",
+
+        }
+    )
+    pwd = PasswordField(
+        label="密码",
+        validators=[
+            DataRequired("请输入密码！")
+        ],
+        description="密码",
+        render_kw={
+            "class": "form-control",
+            "placeholder": "请输入密码！",
+
+        }
+    )
+    submit = SubmitField(
+        '登录',
+        render_kw={
+            "class": "btn btn-primary btn-block btn-flat",
+        }
+    )
+
+    def validate_account(self, field):
+        account = field.data
+        # 查询账号并统计有几条
+        admin = Admin.query.filter_by(name=account).count()
+        if admin == 0:
+            raise ValidationError("账号不存在！")
